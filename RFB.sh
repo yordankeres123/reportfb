5e5ffe1b7acabdbc31508d60688055d4c704ea07
Bigbos starla <48884075+yordankeres123@users.noreply.github.com>
2019-05-05 20:53:09 +0700

    Add files via upload

---
+++ RFB.sh
@@ -0,0 +1,475 @@
+#!/system/xbin/bash
+#mau ngapaib lo anjing mau ngerecode
+#gabakal jadi mastah cok
+#pm gue aja kalo mau ni fb gue
+#Yordan Penceng tu pm aja cuk_-
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+echo
+cd modul
+python2 l.py
+echo
+echo $bi"###################################################"
+echo $bi"#RFB#" $me"Report Fb Akan Dimulai" $bi"######################"
+echo $bi"#RFB#"$bi"##############################################"
+sleep 4
+clear
+echo
+echo $i"Author : Bigbos Starla " $ku"Atau" $i"GREEN DRAGON"
+echo
+sleep 1
+echo $me"Team : " $cy"CYB3R POP1CE +62"
+echo
+sleep 1
+echo $pu"Friends : " "ARUL , MR.GHOST . BANG HANZ ALL TEAM" | lolcat
+echo
+sleep 2
+echo '''\a
+ ____________    __ 	      ___
+ |           \  |  \______   |   |
+ |            \ |         \  |___|
+ |      _____ / |          |
+ |     /        |          | ____
+ |     \_____   |  |______/  |   |
+ |           \  || |         |   |
+ |           /  |  |         |   |
+ |__________/   |__|         |___| '''
+
+sleep 1
+clear
+echo $cy"Silahkan Masukan Idnya Yah Kawan"
+echo $pu"Eh Iya Btw Nama Lo siapa cuk :v"
+read -p "NAMA LO SIAPA : " pukiii
+if [ $pukiii = $pukiii ]
+then
+clear
+echo
+echo $me"Salam Kenal Yah" $pukiii
+sleep 1
+echo "silahkan ke tools report fb nya (RFB)" | lolcat
+sleep 1
+fi
+
+clear
+echo
+sleep 3
+echo "LOADING 100%"
+echo "Work 100%"
+clear
+echo
+echo
+echo $me"Masukan Id Target Untuk direport"
+echo $me"Misal 113xxxxxxxx8 "
+echo
+read -p "Masukan Id Target : " aide
+if [ $aide = $aide ]
+then
+clear
+toilet Report | lolcat
+echo
+echo $i"Sedang Mereport"
+echo $i"Report Ke 1"
+echo
+sleep 1
+echo $i"Sedang Mereport" $aide
+echo $i"Report Ke 2"
+echo
+sleep 2
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 3:"
+sleep 4
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 4:"
+sleep 8
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 5 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 6:"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 7 :"
+sleep 17
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 8 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 9 :"
+sleep 16
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 10 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 11 :"
+sleep 15
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 12 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 13 :"
+sleep 13
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 14 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 15 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 16:"
+sleep 12
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 17 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 18 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 19 :"
+sleep 22
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 20 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 21:"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 22 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 23 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 24 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 25 :"
+sleep 17
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 26 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 27 :"
+sleep 7
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 29 :"
+sleep 18
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 30 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 31 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 32 :"
+sleep 7
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 33 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 34 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 35 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 36 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 37 :"
+sleep 6
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 38 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 39 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 40 :"
+sleep 7
+echo
+apt update
+apt upgrade
+apt install python
+pkg install git
+pkg install report
+pkg install php
+pkg install pip2
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 100 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 140:"
+sleep 3
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 296 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 302 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 359:"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 360 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 378 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 399 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 400:"
+sleep 4
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 528:"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 550 :"
+sleep 5
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 555 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 576 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 590:"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 599 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 600 :"
+sleep 6
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 620 :"
+sleep 7
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 630:"
+sleep 8
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 645 :"
+sleep 9
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 678:"
+sleep 10
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 682:"
+sleep 11
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 686:"
+sleep 12
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 687 :"
+sleep 13
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 699:"
+sleep 14
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 703:"
+sleep 15
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 740 :"
+sleep 16
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 752 :"
+sleep 17
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 759:"
+sleep 18
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 760 :"
+sleep 20
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 789 :"
+sleep 19
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 798 :"
+sleep 8
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 830:"
+sleep 8
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 846 :"
+sleep 7
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 869 :"
+sleep 1
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 879 :"
+sleep 18
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 899 :"
+sleep 19
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 906 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 940 :"
+sleep 4
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 968 :"
+sleep 16
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 970 :"
+sleep 11
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 981 :"
+sleep 10
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 985:"
+sleep 6
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 989 :"
+sleep 7
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 993:"
+sleep 8
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 996:"
+sleep 9
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 997 :"
+sleep 1
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 998 :"
+sleep 2
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 999 :"
+sleep 3
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 1000 :"
+sleep 4
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 1094 :"
+sleep 5
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 1130:"
+sleep 6
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 1150 :"
+sleep 6
+echo
+echo $i"Sedang Mereport" $aide
+echo $i"Report ke 1180 :"
+sleep 9
+fi
+
+echo "Silahkan Ketik yes untuk lanjut" | lolcat
+echo "Ketik N untuk keluar" | lolcat
+read -p "pilih y/n " joyoboyo
+if [ $joyoboyo = Y ] || [ $joyoboyo = y ]
+then
+clear
+figlet Lanjut | lolcat
+echo " Tools Akan Dilanjut"
+sh RFB.sh
+fi
+
+if [ $joyoboyo = N ] || [ $joyoboyo = n ]
+then
+clear
+figlet KELUAR | lolcat
+echo $ku"MAKASIH TELAH MENGGUNAKAN TOOLS KAMI"
+sleep 1
+echo
+echo $me"Author : Bigbos Starla atau " $i"GREEN DRAGON"
+sleep 1
+echo
+echo $bi"SAMPAI JUMPA LAGI"
+exit
+fi
